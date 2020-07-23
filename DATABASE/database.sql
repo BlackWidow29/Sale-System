c3895d366b8ef161daac7711179df79422612080
Isabelly Dias Faustino <isabellydias29@gmail.com>
2020-07-22 21:15:50 -0300

    ALTER TABLES

--- DATABASE/database.sql
+++ DATABASE/database.sql
@@ -8,6 +8,12 @@ CREATE TABLE tblUser(
 	dsStatus ENUM ('ATIVO', 'INATIVO')
 );
 
+ALTER TABLE tblUser
+ADD CONSTRAINT PK_User_User PRIMARY KEY (cdUser);
+
+ALTER TABLE tblUser
+MODIFY cdUser  INTEGER AUTO_INCREMENT;
+
 CREATE TABLE tblLogin 
 (
     cdLogin    INTEGER,
@@ -17,10 +23,26 @@ CREATE TABLE tblLogin
 	cd_User    INTEGER UNIQUE
 );
 
+ALTER TABLE tblLogin
+ADD CONSTRAINT PK_Login PRIMARY KEY (cdLogin);
+
+ALTER TABLE tblLogin
+ADD CONSTRAINT FK_User_Login
+FOREIGN KEY (cd_User) REFERENCES tblUser (cdUser);
+
+ALTER TABLE tblLogin
+MODIFY cdLogin INTEGER AUTO_INCREMENT;
+
 CREATE TABLE tblCashier
 (
     cdCashier INTEGER
 );
+
+ALTER TABLE tblCashier
+ADD CONSTRAINT PK_Cashier_Cashier PRIMARY KEY (cdCashier);
+
+ALTER TABLE tblCashier
+MODIFY cdCashier  INTEGER AUTO_INCREMENT;
 
 CREATE TABLE tblCashierUser
 (
@@ -40,6 +62,9 @@ CREATE TABLE tblAddress
     sgUf       CHAR(2)      NOT NULL DEFAULT 'SP'
 );
 
+ALTER TABLE tblAddress
+ADD CONSTRAINT PK_CEP_Address PRIMARY KEY (noCep);
+
 CREATE TABLE tblProvider
 (
     cdProvider   INTEGER,
@@ -50,6 +75,16 @@ CREATE TABLE tblProvider
     nmEmail      CHAR(100)   NOT NULL,
     noCellphone  NUMERIC(14) NOT NULL
 );
+
+ALTER TABLE tblProvider
+ADD CONSTRAINT PK_Provider_Provider PRIMARY KEY (cdProvider);
+
+ALTER TABLE tblProvider
+MODIFY cdProvider  INTEGER AUTO_INCREMENT;
+
+ALTER TABLE tblProvider
+ADD CONSTRAINT FK_Address_Provider
+FOREIGN KEY (noCEP) REFERENCES tblAddress(noCEP);
 
 CREATE TABLE tblProduct
 (
@@ -63,6 +98,19 @@ CREATE TABLE tblProduct
     noQuantity       INT        NOT NULL
 );
 
+ALTER TABLE tblProduct
+ADD CONSTRAINT PK_Product_Product PRIMARY KEY (cdProduct);
+
+ALTER TABLE tblProduct
+MODIFY cdProduct  INTEGER AUTO_INCREMENT;
+
+CREATE TABLE tblProviderProduct
+(
+    cdProvider INTEGER,
+    cdProduct  INTEGER,
+    PRIMARY KEY(cdProvider,cdProduct) 
+);
+
 CREATE TABLE tblClient
 (
     cdClient     INTEGER,
@@ -74,6 +122,16 @@ CREATE TABLE tblClient
     noCellphone  NUMERIC(14) NOT NULL
 );
 
+ALTER TABLE tblClient
+ADD CONSTRAINT PK_Client_Client PRIMARY KEY (cdClient);
+
+ALTER TABLE tblClient
+MODIFY cdClient  INTEGER AUTO_INCREMENT;
+
+ALTER TABLE tblClient
+ADD CONSTRAINT FK_Address_Client
+FOREIGN KEY (noCEP) REFERENCES tblAddress(noCEP);
+
 CREATE TABLE tblSale
 (
     cdSale          INTEGER,
@@ -83,11 +141,31 @@ CREATE TABLE tblSale
     noTotalValue    DECIMAL(10) NOT NULL
 );
 
+ALTER TABLE tblSale
+ADD CONSTRAINT PK_Sale_Sale PRIMARY KEY (cdSale);
+
+ALTER TABLE tblSale
+MODIFY cdSale  INTEGER AUTO_INCREMENT;
+
+ALTER TABLE tblSale
+ADD CONSTRAINT FK_Login_Sale
+FOREIGN KEY (cd_Login) REFERENCES tblLogin(cdLogin);
+
+ALTER TABLE tblSale
+ADD CONSTRAINT FK_Cashier_Sale
+FOREIGN KEY (cd_Cashier) REFERENCES tblCashier(cdCashier);
+
 CREATE TABLE tblTypePay
 (
     cdTypePay INTEGER,
     nmTypePay ENUM('Cartão de Débito','Cartão de crédito','Dinheiro')
 );
+
+ALTER TABLE tblTypePay
+ADD CONSTRAINT PK_TypePay_TypePay PRIMARY KEY (cdTypePay);
+
+ALTER TABLE tblTypePay
+MODIFY cdTypePay  INTEGER AUTO_INCREMENT;
 
 CREATE TABLE tblTypeSale
 (
@@ -100,6 +178,7 @@ CREATE TABLE tblSaleProduct
 (
     cdSale     INTEGER,
     cdProduct  INTEGER,
+    PRIMARY KEY(cdSale,cdProduct),
     noValue    DECIMAL(8),
     noQuantity INTEGER
 );
@@ -117,21 +196,65 @@ CREATE TABLE tblCompany
     sgUf          CHAR(2)      NOT NULL DEFAULT 'SP'
 );
 
+ALTER TABLE tblCompany
+ADD CONSTRAINT PK_Company_Company PRIMARY KEY (cdCompany);
+
+ALTER TABLE tblCompany
+MODIFY cdCompany  INTEGER AUTO_INCREMENT;
+
 CREATE TABLE tblCoupon
 (
     cdCoupon   INTEGER,
     cd_Sale    INTEGER  NOT NULL,
     cd_Company INTEGER  NOT NULL,
+    cd_Client  INTEGER  NOT NULL,
     dtEmission DATETIME NOT NULL
 );
+
+ALTER TABLE tblCoupon
+ADD CONSTRAINT PK_Coupon_Coupon PRIMARY KEY (cdCoupon);
+
+ALTER TABLE tblCoupon
+MODIFY cdCoupon  INTEGER AUTO_INCREMENT;
+
+ALTER TABLE tblCoupon
+ADD CONSTRAINT FK_Sale_Coupon
+FOREIGN KEY (cd_Sale) REFERENCES tblSale(cdSale);
+
+ALTER TABLE tblCoupon
+ADD CONSTRAINT FK_Company_Coupon
+FOREIGN KEY (cd_Company) REFERENCES tblCompany(cdCompany);
+
+ALTER TABLE tblCoupon
+ADD CONSTRAINT FK_Client_Coupon
+FOREIGN KEY (cd_Client) REFERENCES tblClient(cdClient);
 
 CREATE TABLE tblPayment
 (
 	cdPay      	   INTEGER,
-    cd_TypePay 	   INTEGER,
-    cd_Sale    	   INTEGER,
-    noParcialValue DECIMAL(10)
+    cd_Client     INTEGER,
+    cd_TypePay 	   INTEGER     NOT NULL,
+    cd_Sale    	   INTEGER     NOT NULL,
+    noParcialValue DECIMAL(10) NOT NULL
 );
+
+ALTER TABLE tblPayment
+ADD CONSTRAINT PK_Payment_Payment PRIMARY KEY (cdPay);
+
+ALTER TABLE tblPayment
+MODIFY cdPay  INTEGER AUTO_INCREMENT;
+
+ALTER TABLE tblPayment
+ADD CONSTRAINT FK_Client_Payment
+FOREIGN KEY (cd_Client) REFERENCES tblClient(cdClient);
+
+ALTER TABLE tblPayment
+ADD CONSTRAINT FK_TypePay_Payment
+FOREIGN KEY (cd_TypePay) REFERENCES tblTypePay(cdTypePay);
+
+ALTER TABLE tblPayment
+ADD CONSTRAINT FK_Sale_Payment
+FOREIGN KEY (cd_Sale) REFERENCES tblSale(cdSale);
 
 
 
