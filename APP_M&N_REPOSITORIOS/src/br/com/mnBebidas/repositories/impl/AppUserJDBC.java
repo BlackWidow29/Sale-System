ce979fea68f1d4264fb20144f099b811c7e4cd48
Isabelly Dias Faustino <isabellydias29@gmail.com>
2020-07-23 20:42:51 -0300

    Método selecionar usuários

--- APP_M&N_REPOSITORIOS/src/br/com/mnBebidas/repositories/impl/AppUserJDBC.java
+++ APP_M&N_REPOSITORIOS/src/br/com/mnBebidas/repositories/impl/AppUserJDBC.java
@@ -2,18 +2,30 @@ package br.com.mnBebidas.repositories.impl;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 import br.com.mnBebidas.repositories.interfaces.AppRepositorio;
 import br.com.mnBedidas.entitys.User;
 
-public class AppJDBC implements AppRepositorio<User> {
+public class AppUserJDBC implements AppRepositorio<User> {
 
 	@Override
 	public List<User> selecionar() throws SQLException {
 		Connection con = null;
-		con = DriverManager.getConnection("jdbc:mysql://localhost:3306/dbpdv","root", "Dias042012");
+		con = DriverManager.getConnection("jdbc:mysql://localhost:3306/dbpdv", "root", "Dias042012");
+		Statement comando = con.createStatement();
+		ResultSet rs = comando.executeQuery("SELECT * FROM tbllogin, tbluser");
+
+		while (rs.next()) {
+			User user = new User();
+			user.setId(rs.getInt("cdLogin"));
+			user.setEmail(rs.getString("nmEmail"));
+			user.setLogin(rs.getString("nmUser"));
+			user.setPassword(rs.getString("nmPassword"));
+		}
 		return null;
 	}
 
